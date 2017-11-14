1787ee26812b106c7bb0e6f32a40d4b8eee45e98
solufemi91 <stevenoo@spartaglobal.com>
2017-11-14 22:05:30 +0000

    added the power and square root

--- main.rb
+++ main.rb
@@ -10,6 +10,8 @@ if users_response == "basic"
   puts "Please choose one of the following operations: *, /, +, -"
   operation = gets.chomp
 
+  ## basic calculator
+
   if operation == "+"
     sum = first_number + second_number
     puts "The answer is #{sum}"
@@ -26,6 +28,25 @@ if users_response == "basic"
 
 
 
-else puts "OK"
+elsif users_response == "advanced"
+  puts "Choose an operation. SquareRoot or Power"
+  operation = gets.chomp
+  if operation == "SquareRoot"
+    puts "enter the number that you want to find the square root of"
+    number_to_be_squared = gets.chomp.to_i
+    answer = Math.sqrt(number_to_be_squared)
+    puts "The answer is #{answer}"
+  elsif operation == "Power"
+    puts "Enter the base number"
+    base = gets.chomp.to_i
+    puts "Enter the exponent "
+    exponent = gets.chomp.to_i
+    answer = base ** exponent
+    puts "The answer is #{answer}"
+
+
+  end
+
+elsif users_response == "bmi"
 
 end
